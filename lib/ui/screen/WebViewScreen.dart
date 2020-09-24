63fa77c4456a8493ca226be47cc9de9993d19b60
山下 育男 <i-yamashita@i-enter.co.jp>
2020-09-24 18:37:12 +0900

    （integration testが動かないので）公式のwebview_flutterに変更

--- lib/ui/screen/WebViewScreen.dart
+++ lib/ui/screen/WebViewScreen.dart
@@ -1,20 +1,8 @@
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_api_sample/ui/WidgetKey.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-
-const kAndroidUserAgent =
-    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
-
-// ignore: prefer_collection_literals
-final Set<JavascriptChannel> jsChannels = [
-  JavascriptChannel(
-      name: 'Print',
-      onMessageReceived: (JavascriptMessage message) {
-        print(message.message);
-      }),
-].toSet();
-
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
   WebViewScreen({Key key, @required this.urlString}) : super(key: key);
@@ -26,29 +14,13 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
 
-  @override
-  Widget build(BuildContext context) {
-    var widget = context.widget as WebViewScreen;
-    return WebViewScreenPage(urlString: widget.urlString);
-  }
-
-}
-
-class WebViewScreenPage extends StatelessWidget {
-  WebViewScreenPage({Key key, @required this.urlString}) : super(key: key);
-  final String urlString;
-  final flutterWebViewPlugin = FlutterWebviewPlugin();
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
 
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-      key: Key(WidgetKey.KEY_WEB_WEB_VIEW),
-      url: urlString,
-      javascriptChannels: jsChannels,
-      mediaPlaybackRequiresUserGesture: false,
+    return Scaffold(
       appBar: AppBar(
-        title: const Text(""),
-        backgroundColor: Colors.greenAccent,
+        title: const Text('記事詳細'),
         leading: IconButton(
           key: Key(WidgetKey.KEY_WEB_APP_BAR_ICON_BUTTON),
           icon: const Icon(
@@ -56,41 +28,87 @@ class WebViewScreenPage extends StatelessWidget {
             key: Key(WidgetKey.KEY_WEB_APP_BAR_ICON),
           ),
           onPressed: () => Navigator.pop(context),
-        )
-      ),
-      withZoom: true,
-      withLocalStorage: true,
-      hidden: true,
-      initialChild: Container(
-        color: Colors.white60,
-        child: const Center(
-          child: CircularProgressIndicator(),
         ),
       ),
+      body: Builder(builder: (BuildContext context) {
+        return WebView(
+          initialUrl: widget.urlString,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          javascriptChannels: <JavascriptChannel>[
+            _toasterJavascriptChannel(context),
+          ].toSet(),
+          navigationDelegate: (NavigationRequest request) {
+            print('allowing navigation to $request');
+            return NavigationDecision.navigate;
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
+        );
+      }),
       bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              onPressed: () {
-                flutterWebViewPlugin.goBack();
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.arrow_forward_ios),
-              onPressed: () {
-                flutterWebViewPlugin.goForward();
-              },
-            ),
-            IconButton(
-              icon: const Icon(Icons.autorenew),
-              onPressed: () {
-                flutterWebViewPlugin.reload();
-              },
-            ),
-          ],
+        child: FutureBuilder<WebViewController>(
+            future: _controller.future,
+            builder: (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+              final bool webViewReady = snapshot.connectionState == ConnectionState.done;
+              final WebViewController snapshotData = snapshot.data;
+              return Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back_ios),
+                      onPressed: !webViewReady ? null : () async {
+                        if (await snapshotData.canGoBack()) {
+                          await snapshotData.goBack();
+                        } else {
+                          Scaffold.of(context).showSnackBar(
+                            const SnackBar(content: Text("履歴がありません")),
+                          );
+                          return;
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_forward_ios),
+                      onPressed: !webViewReady ? null : () async {
+                        if (await snapshotData.canGoForward()) {
+                          await snapshotData.goForward();
+                        } else {
+                          Scaffold.of(context).showSnackBar(
+                            const SnackBar(content: Text("履歴がありません")),
+                          );
+                          return;
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.autorenew),
+                      onPressed: !webViewReady ? null : () {
+                        snapshotData.reload();
+                      },
+                    ),
+                  ],
+              );
+            }
         ),
       ),
     );
   }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+
 }
